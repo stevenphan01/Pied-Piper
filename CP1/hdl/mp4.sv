d3e7a48f00901d6c531c0698a659edb6a40075f1
Steven Phan <sphan5@illinois.edu>
2021-10-26 18:00:35 -0500

    CP1 Done

---
+++ CP1/hdl/mp4.sv
@@ -0,0 +1,24 @@
+import rv32i_types::*; 
+module mp4 (
+    input clk,
+    input rst,
+    /* Instruction memory interface */ 
+    input  inst_resp,
+    input  rv32i_word inst_rdata, 
+    output inst_read, 
+    output rv32i_word inst_addr, 
+    /* Data memory interface */
+    input data_resp, 
+    input rv32i_word data_rdata, 
+    output data_read,
+    output data_write, 
+    output [3:0] data_mbe, 
+    output rv32i_word data_addr, 
+    output rv32i_word data_wdata
+);
+/* Signals needed for RVFI Monitor */
+
+/* Declare Datapath */
+datapath datapath(.*);
+
+endmodule : mp4
