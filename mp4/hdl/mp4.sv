c0f16f85c371d67a49585b1ba3bfad9d88d285ee
Steven Phan <sphan5@illinois.edu>
2021-11-27 13:25:19 -0600

    added eviction buffer to master branch

--- mp4/hdl/mp4.sv
+++ mp4/hdl/mp4.sv
@@ -1,4 +1,4 @@
-import rv32i_types::*; 
+import rv32i_types::*;
 module mp4 (
     input clk,
     input rst,
@@ -32,13 +32,21 @@ logic inst_pmem_read;
 logic inst_pmem_resp; 
 rv32i_line inst_pmem_rdata; 
 
-/* Between arbiter and data cache */
+/* Between ev buffer and data cache */
 rv32i_word data_pmem_address; 
 logic data_pmem_read;
 logic data_pmem_write;
 rv32i_line data_pmem_wdata;
+logic ev_resp; 
+rv32i_line ev_rdata;
+
+/* Between arbiter and ev buffer */
 logic data_pmem_resp;
 rv32i_line data_pmem_rdata; 
+rv32i_word ev_address; 
+logic ev_write; 
+logic ev_read; 
+rv32i_line ev_wdata; 
 
 /* Declare Datapath */
 datapath datapath(.*);
@@ -49,10 +57,10 @@ arbiter arbiter (
     .inst_pmem_read(inst_pmem_read), 
     .inst_pmem_rdata(inst_pmem_rdata), 
     .inst_pmem_resp(inst_pmem_resp),
-    .data_pmem_address(data_pmem_address),
-    .data_pmem_read(data_pmem_read), 
-    .data_pmem_write(data_pmem_write),
-    .data_pmem_wdata(data_pmem_wdata), 
+    .data_pmem_address(ev_address),
+    .data_pmem_read(ev_read), 
+    .data_pmem_write(ev_write),
+    .data_pmem_wdata(ev_wdata), 
     .data_pmem_rdata(data_pmem_rdata), 
     .data_pmem_resp(data_pmem_resp),
     .pmem_rdata(mem_rdata), 
@@ -61,6 +69,40 @@ arbiter arbiter (
     .pmem_read(mem_read),
     .pmem_wdata(mem_wdata), 
     .pmem_addr(mem_addr)
+);
+
+ev_buffer ev_buffer(
+  .clk(clk),
+  .rst(rst), 
+  .data_pmem_address(data_pmem_address),
+  .data_pmem_write(data_pmem_write), 
+  .data_pmem_read(data_pmem_read), 
+  .data_pmem_wdata(data_pmem_wdata),
+  .ev_resp(ev_resp), 
+  .ev_rdata(ev_rdata), 
+  .data_pmem_resp(data_pmem_resp), 
+  .data_pmem_rdata(data_pmem_rdata), 
+  .ev_address(ev_address),
+  .ev_write(ev_write), 
+  .ev_read(ev_read), 
+  .ev_wdata(ev_wdata)
+);
+
+cache data_cache (
+  .clk(clk),
+  .pmem_resp(ev_resp),
+  .pmem_rdata(ev_rdata),
+  .pmem_address(data_pmem_address),
+  .pmem_wdata(data_pmem_wdata),
+  .pmem_read(data_pmem_read),
+  .pmem_write(data_pmem_write),
+  .mem_read(data_read_dp),
+  .mem_write(data_write_dp),
+  .mem_byte_enable_cpu(data_mbe_dp),
+  .mem_address(data_addr_dp),
+  .mem_wdata_cpu(data_wdata_dp),
+  .mem_resp(data_resp_dp),
+  .mem_rdata_cpu(data_rdata_dp)
 );
 
 cache instr_cache(
@@ -78,23 +120,6 @@ cache instr_cache(
   .mem_wdata_cpu(32'd0),
   .mem_resp(inst_resp_dp),
   .mem_rdata_cpu(inst_rdata_dp)
-);
-
-cache data_cache (
-  .clk(clk),
-  .pmem_resp(data_pmem_resp),
-  .pmem_rdata(data_pmem_rdata),
-  .pmem_address(data_pmem_address),
-  .pmem_wdata(data_pmem_wdata),
-  .pmem_read(data_pmem_read),
-  .pmem_write(data_pmem_write),
-  .mem_read(data_read_dp),
-  .mem_write(data_write_dp),
-  .mem_byte_enable_cpu(data_mbe_dp),
-  .mem_address(data_addr_dp),
-  .mem_wdata_cpu(data_wdata_dp),
-  .mem_resp(data_resp_dp),
-  .mem_rdata_cpu(data_rdata_dp)
 );
 
 endmodule : mp4
